b3a2daa9e4d5161e428b6d17105515d4fa2a9b97
ben cravis <ben.conor.reese.cravens@gmail.com>
2020-05-21 19:19:41 +1200

    sped up conj_grad.py quite a bit

--- conj_grad.py
+++ conj_grad.py
@@ -1,8 +1,10 @@
 import numpy as np
 from numpy import linalg as la
 from sklearn.covariance import EmpiricalCovariance as Ecov
+import time
 
-def conj_grad(A,k,err_tol):
+@profile
+def conj_grad(A,b,k):
     """perform conjugate gradient descent on Ax=b until 
     error tolerance is reached"""
     [m,n] = np.shape(A)
@@ -12,51 +14,56 @@ def conj_grad(A,k,err_tol):
     y = x
     #initialize residual
     r = b - np.dot(A,x)
+    #initialize transpose of residual
+    rsold = np.dot(np.transpose(r),r)
     #initialize search direction
     p = r
+    #calculate A*p vector
+    A_p = np.dot(A,p)
     #normalizing constant
     d = np.dot(np.transpose(p),np.dot(A,p))
+    #random vectors
+    z = np.random.randn(k)
     #iterate k times (or until convergence)
     for i in range(k):
-        gamma = (np.dot(np.transpose(r),r))/d
+        gamma = (rsold)/d
         x = x + np.dot(p,gamma)
-        #sample from N(0,1)
-        z = np.random.randn()
         #update sample 
-        y = y + (z/np.sqrt(d))*p
+        y = y + (z[i]/np.sqrt(d))*p
         #store old r to calculate new beta
-        r_old = r
-        r = r - gamma*np.dot(A,p)
+        r = r - gamma*A_p
+        rsnew = np.dot(np.transpose(r),r)
+        #is the magnitude of the residual < 1e-16?
+        if np.sqrt(rsnew) < 1e-8:
+            print("converged at iteration {}/{}".format(i,k))
+            break
         #calculate new beta
-        beta_num = -np.dot(np.transpose(r),r)
-        beta_denom = np.dot(np.transpose(r_old),r_old)
-        beta = beta_num/beta_denom
+        beta = -rsnew/rsold
+        #update rsold constant
+        rsold = rsnew
         #calculate new search direction
         p = r - beta*p
+        #calculate A*p vector
+        A_p = np.dot(A,p)
         #calculate new normalization constant
-        d = np.dot(np.transpose(p),np.dot(A,p))
-        if la.norm(r) < err_tol:
-            return [x,y]
+        d = np.dot(np.transpose(p),A_p)
     return [x,y]
 
 if __name__=="__main__":
-    #A = np.asarray([[1,0.1,0.1],[0.1,1,0.1],[0.1,0.1,1]])
+    t_0 = time.time()
     A = np.loadtxt("res.txt",delimiter=',')
+    dims = np.shape(A)[0]
     print(np.shape(A))
-    b = np.ones([3,1])
-    k = 3
-    err_tol = 2.22e-16
-    sample_num = 50000
-    samples = []
-    for i in range(sample_num):
-        [x,y] = conj_grad(A,b,k,err_tol)
-        samples.append(y)
+    b = np.ones([dims,1])
+    k = dims 
+    sample_num = int(1e4)
+    samples = np.asarray([conj_grad(A,b,k)[1] for i in range(sample_num)])
     samples = np.squeeze(samples,axis=2)
+    print(np.shape(samples))
     e_cov = Ecov().fit(samples).covariance_
     real_cov = la.inv(A)
     print(e_cov)
     print(real_cov)
-    print(la.norm(e_cov - real_cov)/(la.norm(real_cov)))
-
-    """perform conjugate gradient descent on Ax=b until 
-    error tolerance is reached"""
+    print("relative error is {}".format(la.norm(e_cov - real_cov)/(la.norm(real_cov))))
+    t_f = time.time()
+    print("{} seconds elapsed".format(t_f - t_0))
--- makefile
+++ makefile
@@ -1,3 +1,8 @@
-run:
+cheby:
 	python3 gibbs_cheby.py > output.txt
 	more output.txt
+cg:
+	rm profile.txt
+	kernprof -l conj_grad.py	
+	python3 -m line_profiler *.lprof >> profile.txt
+	more profile.txt
